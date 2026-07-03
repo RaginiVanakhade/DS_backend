30666c48084b26fe17da471388f7f672922c0811
Ragini Vanakhade <raginivankhade1203@gmail.com>
2026-07-03 18:36:47 +0530

    added file upload functionality

--- src/routes/authroutes.ts
+++ src/routes/authroutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { register } from "../controllers/authcontroller";
+import {upload} from "../uploads/upload"
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", upload.single("profilePicture"), register);
 
 export default router;
