bfda02c855e887f299e0f2bf00d335800a770260
Ragini Vanakhade <raginivankhade1203@gmail.com>
2026-07-03 20:53:12 +0530

    added login endpoint

--- src/controllers/authcontroller.ts
+++ src/controllers/authcontroller.ts
@@ -1,5 +1,7 @@
 import User from "../models/users";
 
+
+// register
 export const register = async (req: any, res: any) => {
   try {
     const user = await User.create(req.body);
@@ -13,6 +15,47 @@ export const register = async (req: any, res: any) => {
     res.status(500).json({
       success: false,
       message: "Something went wrong",
+    });
+  }
+};
+
+
+// login 
+
+export const login = async (req: any, res: any) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    // Check password
+    if (user.password !== password) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Login Successful",
+      data: user,
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
     });
   }
 };
--- src/routes/authroutes.ts
+++ src/routes/authroutes.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { register } from "../controllers/authcontroller";
+import { register, login } from "../controllers/authcontroller";
 import {upload} from "../uploads/upload"
 
 const router = express.Router();
 
 router.post("/register", upload.single("profilePicture"), register);
-
+router.post("/login", login);
 export default router;
